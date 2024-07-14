fa08e658dd249545d184d0f44218194da43ce4e3
Jayavalli V <127413212+Jaya-MCA@users.noreply.github.com>
2024-07-14 22:35:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function showEat() {
+    document.getElementById('eat').style.display = 'block';
+    document.getElementById('Drinks').style.display = 'none';
+}
+
+function showDrinks() {
+    document.getElementById('eat').style.display = 'none';
+    document.getElementById('Drinks').style.display = 'block';
+}
+
